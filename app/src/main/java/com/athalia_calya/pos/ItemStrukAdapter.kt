97d9a08e310b66390c5b4c14ff52fd508a6aa426
Athalia <brigittaathaliac@gmail.com>
2026-06-03 11:44:16 +0700

    update

---
+++ app/src/main/java/com/athalia_calya/pos/ItemStrukAdapter.kt
@@ -0,0 +1,44 @@
+package com.athalia_calya.pos
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.athalia_calya.pos.databinding.ItemStrukTransaksiBinding
+import com.athalia_calya.pos.model.ItemTransaksi
+import java.text.NumberFormat
+import java.util.Locale
+
+class ItemStrukAdapter(
+    private val items: ArrayList<ItemTransaksi>
+) : RecyclerView.Adapter<ItemStrukAdapter.StrukViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StrukViewHolder {
+        val binding = ItemStrukTransaksiBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return StrukViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: StrukViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount() = items.size
+
+    inner class StrukViewHolder(
+        private val binding: ItemStrukTransaksiBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: ItemTransaksi) {
+            binding.tvNamaProduk.text = item.namaProduk.uppercase()
+            binding.tvJumlah.text = "x${item.jumlah}"
+            binding.tvHarga.text = "Rp " + formatNumber(item.harga.toLong())
+            binding.tvSubtotal.text = "Rp " + formatNumber(item.subtotal)
+        }
+
+        private fun formatNumber(amount: Long): String {
+            val formatter = NumberFormat.getInstance(Locale("id", "ID"))
+            return formatter.format(amount)
+        }
+    }
+}
